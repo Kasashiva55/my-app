151c4de4e8896ef1c9381f7cae32e771c9b9408b
Kasashiva55 <96681710+Kasashiva55@users.noreply.github.com>
2024-07-17 17:39:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
           <a class="navbar-brand" href="/">
-          <img src={logo} alt="Bootstrap" width="240" height="65"/>
+          <img src={logo} alt="logo" width="85%" height="85%"/>
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
